0ef9c8d2dde9605915b605fb0896d73afe21af62
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-04-07 15:57:58 -0400

    Added LSB comment header to dns init script

--- services/dns/initscript.sh
+++ services/dns/initscript.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/twistd-service
 
+### BEGIN INIT INFO
+# Provides:          dns
+# Required-Start:    $syslog $named $network $time
+# Required-Stop:     $syslog $named $network
+# Should-Start:
+# Should-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Twisted DNS server
+# Description:       dns is a DNS server based on Twisted Names
+### END INIT INFO
 
 TWISTD='authbind --deep twistd'
 SERVICE='dns'
