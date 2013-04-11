d4c9f3f0cc5430686ea81cac8f87f04b46d975b5
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-04-11 17:29:52 -0400

    Initial support for postgres

---
+++ fablib/postgres.py
@@ -0,0 +1,14 @@
+from fablib import package
+
+
+def install():
+    package.install('postgresql-9.1')
+    package.install('postgresql-server-dev-9.1')
+
+
+def createuser(name):
+    pass
+
+
+def createdb(name, owner):
+    pass
