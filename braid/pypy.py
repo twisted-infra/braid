5887ae97ff09b447b89f9fadd911eae05e424ed5
Tom Prince <tom.prince@ualberta.net>
2013-04-22 11:25:41 -0600

    Don't create pypy user if it already exists.

--- braid/pypy.py
+++ braid/pypy.py
@@ -1,6 +1,7 @@
 from os import path
 from fabric.api import cd, task, sudo
 
+from braid import fails
 
 pypyURL = 'https://bitbucket.org/pypy/pypy/downloads/pypy-2.0-beta2-linux64-libc2.15.tar.bz2'
 setuptoolsURL = 'http://peak.telecommunity.com/dist/ez_setup.py'
@@ -12,9 +13,10 @@ pypyDir = '/opt/pypy-2.0-beta2'
 @task
 def install():
     sudo('mkdir -p /opt')
-    sudo('useradd --home-dir {} --gid bin '
-         '-M --system --shell /bin/false '
-         'pypy'.format(pypyDir))
+    if fails('id {}'.format('pypy')):
+        sudo('useradd --home-dir {} --gid bin '
+             '-M --system --shell /bin/false '
+             'pypy'.format(pypyDir))
 
     with cd('/opt'):
         for url in pypyURL, setuptoolsURL, pipURL:
