30d6d1c5bd72f4460133a8055d17e56305afbe42
Tom Prince <tom.prince@ualberta.net>
2013-04-24 10:49:32 -0600

    Add fedora support to braid.pip.install.

--- braid/pip.py
+++ braid/pip.py
@@ -1,9 +1,16 @@
 from fabric.api import run
 
+from braid import info
+
 
 def install(package, python='pypy'):
+    pip = 'pip'
     if python == 'pypy':
         pip = '~pypy/bin/pip'
-    else: #FIXME https://github.com/twisted-infra/braid/issue/5
-        pip = 'pip'
+    elif python == 'system':
+        #FIXME https://github.com/twisted-infra/braid/issue/5
+        if info.distroFamily() == 'fedora':
+            pip = 'pip-python'
+        else:
+            pip = 'pip'
     run('{} install --user {}'.format(pip, package))
