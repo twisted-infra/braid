00946fb99b0aa6ada881f1362faab1c68bd3b545
Tom Prince <tom.prince@ualberta.net>
2013-04-25 16:58:21 -0600

    Add support for launchpad keys.

--- braid/users.py
+++ braid/users.py
@@ -1,4 +1,7 @@
 from __future__ import print_function, absolute_import
+
+import requests
+
 from fabric.api import task, sudo
 from fabric.contrib import files
 
@@ -14,15 +17,29 @@ def create(username, homeBase='/home'):
                 '--shell /bin/bash {}'.format(homeBase, username))
 
 
+def uploadKeys(user, keys):
+    """
+    Uplaod a list of keys to a user's autorized_keys file.
+    """
+    sudo('mkdir -p ~{}/.ssh'.format(user))
+    files.append('~{}/.ssh/authorized_keys'.format(user), keys, use_sudo=True)
+
+
 @task
-def uploadKeys(user, keyfile):
+def uploadKeyFile(user, keyfile):
     """
     Add keys from an authorized_keys file to a given user.
     """
     # FIXME: Detect whether we are already connecting as the user who's
     # keys we are modifying, and don't use sudo then.
     keys = FilePath(keyfile).getContent().splitlines()
-    sudo('mkdir -p ~{}/.ssh'.format(user))
-    files.append('~{}/.ssh/authorized_keys'.format(user), keys, use_sudo=True)
+    uploadKeys(user, keys)
 
 
+@task
+def uploadLaunchpadKeys(user, launchpadUser=None):
+    if launchpadUser is None:
+        launchpadUser = user
+    r = requests.get('https://launchpad.net/~{}/+sshkeys'.format(launchpadUser))
+    keys = r.text.splitlines()
+    uploadKeys(user, keys)
