c326cec791b076ce95b5cf2c0bdf7a0ab838efba
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-04 12:34:37 -0400

    pep8

--- braid/postgres.py
+++ braid/postgres.py
@@ -6,21 +6,25 @@ from pipes import quote
 def install():
     package.install(['postgresql-9.1', 'postgresql-server-dev-9.1'])
 
+
 def _runQuery(query):
     with quiet():
         return sudo('psql --no-align --no-readline --no-password --quiet '
                     '--tuples-only -c {}'.format(quote(query)),
                     user='postgres', pty=False, combine_stderr=False)
 
+
 def _dbExists(name):
     res = _runQuery("select count(*) from pg_database "
                     "where datname = '{}';".format(name))
     return res == '1'
 
+
 def _userExists(name):
     res = _runQuery("select count(*) from pg_user "
                     "where usename = '{}';".format(name))
     return res == '1'
+
 
 def createUser(name):
     if not _userExists(name):
@@ -29,4 +33,5 @@ def createUser(name):
 
 def createDb(name, owner):
     if not _dbExists(name):
-        sudo('createdb -O {} {}'.format(owner, name), user='postgres', pty=False)
+        sudo('createdb -O {} {}'.format(owner, name), user='postgres',
+             pty=False)
