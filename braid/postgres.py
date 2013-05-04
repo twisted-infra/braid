a0b2ed743012ae3a2ae86baf1671f25aa584a106
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-04 14:41:23 -0400

    Implement privileges grant functions

--- braid/postgres.py
+++ braid/postgres.py
@@ -38,13 +38,34 @@ def createDb(name, owner):
              pty=False)
 
 
-def grantRead(user, database):
+def _grantSchemaAccess(database, user):
+    # Double quotes tell postgres to consider names as case sensitive. If we
+    # omit them and have uppercase chars in the names, the relevant object
+    # (user or database) will not be found.
+    _runQuery('grant connect on database "{}" to "{}";'.format(database, user))
+    _runQuery('grant usage on schema public to "{}";'.format(user), database)
+
+
+def grantRead(database, user):
     """
     Grant read permissions to C{user} to all tables in C{database}.
     """
+    _grantSchemaAccess(database, user)
+    # This only affects existing tables. It is possible to set the default
+    # privileges on the schema for new objects but this rapidly complicates
+    # everything. The simple solution is to just re-run this command
+    _runQuery('grant select on all tables in schema public to "{}";'
+              .format(user), database)
 
 
-def grantReadWrite(user, database):
+def grantReadWrite(database, user):
     """
     Grant read and write permissions to C{user} to all tables in C{database}.
     """
+    _grantSchemaAccess(database, user)
+    # TODO: We may want to change the following to grant only a limited set of
+    # privileges.
+    _runQuery('grant all privileges on all tables in schema public to "{}";'
+              .format(user), database)
+    _runQuery('grant all privileges on all sequences in schema public to "{}";'
+              .format(user), database)
