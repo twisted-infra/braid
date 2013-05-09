f9e8cc51996b1827fae6e05041ca26b1f94f2698
Tom Prince <tom.prince@ualberta.net>
2013-05-09 13:26:27 -0600

    Add some support for generating equivs packages.

---
+++ braid/debian/equivs.py
@@ -0,0 +1,24 @@
+from cStringIO import StringIO
+from twisted.python.filepath import FilePath
+from fabric.api import cd, sudo
+from braid.utils import tempfile
+
+def _generateControlFile(package, provides, description=None):
+    """
+    Generate a control file for C{package}.
+
+    @returns: the control file
+    @rtype: L{file}-like
+    """
+    if description is None:
+        description = 'Dummy provider of %s.' % provides
+
+    template = FilePath(__file__).sibling('equivs.control').getContent()
+    body = template.format(package=package, provides=provides, description=description)
+    return StringIO(body)
+
+def installEquiv(package, provides, description=None):
+    control = _generateControlFile(package, provides, description)
+    with tempfile(control) as controlFile, cd('/root'):
+        sudo('equivs-build {}'.format(controlFile))
+        sudo('dpkg -i {}_1.0_all.deb'.format(package))
