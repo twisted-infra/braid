e936c2daada6592963de1f4906f9e9410921448e
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-05-09 00:21:15 -0400

    Publish the postgres module as first-level tasks.

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,8 @@ infrastructure. Use this utility to install, update and start/stop/restart
 services running on twistedmatrix.com.
 """
 
-"""
-This file is a simple entry point, nothing is final about it!
-Just experimenting for now.
-"""
+
+from braid import base, users, postgres, config
 
 
-from braid import base, users
-from braid import config
-
-
-__all__ = ['base', 'config', 'users']
+__all__ = ['base', 'config', 'users', 'postgres']
