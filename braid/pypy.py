70c3b4f6977fba1db0d1e37740b49463259ff24c
Tom Prince <tom.prince@ualberta.net>
2013-05-10 10:22:34 -0600

    Update pypy to released version of 2.0.
    Fixes #27.

--- braid/pypy.py
+++ braid/pypy.py
@@ -3,12 +3,11 @@ from fabric.api import cd, task, sudo
 
 from braid import fails
 
-pypyURL = 'https://bitbucket.org/pypy/pypy/downloads/pypy-2.0-beta2-linux64-libc2.15.tar.bz2'
+pypyURL = 'https://bitbucket.org/pypy/pypy/downloads/pypy-2.0-linux64.tar.bz2'
 setuptoolsURL = 'http://peak.telecommunity.com/dist/ez_setup.py'
 pipURL = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
 
-pypyDir = '/opt/pypy-2.0-beta2'
-
+pypyDir = '/opt/pypy-2.0'
 
 @task
 def install():
@@ -17,6 +16,8 @@ def install():
         sudo('useradd --home-dir {} --gid bin '
              '-M --system --shell /bin/false '
              'pypy'.format(pypyDir))
+    else:
+        sudo('usermod --home-dir {} pypy'.format(pypyDir))
 
     with cd('/opt'):
         for url in pypyURL, setuptoolsURL, pipURL:
