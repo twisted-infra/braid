7813677047d5793ff0d536fdd23cc10de8edaa04
Tom Prince <tom.prince@ualberta.net>
2013-05-09 18:00:07 -0600

    Add some simplistic debconf support.

---
+++ braid/debian/debconf.py
@@ -0,0 +1,8 @@
+from cStringIO import StringIO
+from fabric.api import sudo
+from braid.utils import tempfile
+
+def setDebconfValue(package, question, type, value):
+    selections = StringIO(' '.join([package, question, type, value]))
+    with tempfile(selections) as controlFile:
+        sudo('debconf-set-selections {}'.format(controlFile))
