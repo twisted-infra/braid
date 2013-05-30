b644ae080c28db87b044b36717ee290915f34c37
Tom Prince <tom.prince@ualberta.net>
2013-05-29 22:03:00 -0600

    Add support for installing packages on fedora.

--- braid/package.py
+++ braid/package.py
@@ -1,8 +1,14 @@
-from fabric.api import sudo
+from braid.api import sudo, abort
 
+from braid.info import distroFamily
 
 def install(packages):
     """
     Install a list of packages.
     """
-    sudo('/usr/bin/apt-get --yes --quiet install {}'.format(" ".join(packages)))
+    if distroFamily() == 'debian':
+        sudo('/usr/bin/apt-get --yes --quiet install {}'.format(" ".join(packages)))
+    elif distroFamily() == 'fedora':
+        sudo('/usr/bin/yum install -y {}'.format(" ".join(packages)))
+    else:
+        abort('Unknown distro.')
