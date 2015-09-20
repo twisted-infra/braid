ecef12e0c5c89f7bb45d76c358e1bb370e17d566
HawkOwl <hawkowl@atleastfornow.net>
2015-09-20 22:03:33 +0800

    only pin cryptography on the pypy builder

--- services/buildbot/master/twisted_factories.py
+++ services/buildbot/master/twisted_factories.py
@@ -29,7 +29,6 @@ FORCEGC_FLAGS = ["--force-gc"]
 
 # Dependencies that work on both CPython 2.7 + 3.3 + 3.4
 BASE_DEPENDENCIES = [
-    'cryptography<=0.9',
     'pyopenssl',
     'service_identity',
     'zope.interface',
