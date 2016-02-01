694f74eeffcf2581dfefe466cdb1f6a6fa43e2d9
Adi Roiban <adi.roiban@chevah.com>
2016-02-01 11:02:03 +0000

    Initial buildbot env.

--- braid/settings.py
+++ braid/settings.py
@@ -3,7 +3,6 @@ Default configuration settings.
 """
 
 dornkirk = 'dornkirk.twistedmatrix.com'
-
 vagrant_address = '172.16.255.140'
 
 ENVIRONMENTS = {
@@ -22,5 +21,13 @@ ENVIRONMENTS = {
         },
         'user': 'root',
         'installPrivateData': False,
+    },
+    'buildbot': {
+        'hosts': ['buildbot.twistedmatrix.com'],
+        'roledefs': {
+            'nameserver': ['buildbot.twistedmatrix.com'],
+        },
+        'user': 'root',
+        'installPrivateData': True,
     }
 }
